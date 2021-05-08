0f4bc686c6db558ba5cfb2540f43b0cfedef8b0e
KevinGosal <s11810042@student.unklab.ac.id>
2021-05-08 21:51:11 +0800

    Kevin Gosal App.js dan router

---
+++ src/pages/AppScreen/index.js
@@ -0,0 +1,4 @@
+import React, {Component} from 'react';
+import { View, Text, AsyncStorage, StyleSheet, TouchableOpacity, ScrollView, TextInput,
+  Keyboard, ActivityIndicator, } from 'react-native';
+
---
+++ src/pages/SplashScreen/index.js
@@ -0,0 +1,4 @@
+import React, {useEffect} from 'react';
+import {StyleSheet, Text, View, Image} from 'react-native';
+import Logo from '../../assets/logo.jpeg'
+
